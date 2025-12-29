0c2ee3e81b8e7c5c64b77a1900b86da9f854869c
Arad <arad.sheybak@gmail.com>
2025-12-29 22:40:45 +0330

    fix: LastNameChanges renamed to LastNameChanged in SignupIntent

--- app/src/main/java/com/aradsheybak/goodfood/screens/signup/presentation/contract/SignupIntent.kt
+++ app/src/main/java/com/aradsheybak/goodfood/screens/signup/presentation/contract/SignupIntent.kt
@@ -2,7 +2,7 @@ package com.aradsheybak.goodfood.screens.signup.presentation.contract
 
 sealed class SignupIntent {
     data class FirstNameChanged(val firstName: String) : SignupIntent()
-    data class LastNameChanges(val lastName: String) : SignupIntent()
+    data class LastNameChanged(val lastName: String) : SignupIntent()
     data class EmailChanged(val email: String) : SignupIntent()
     data class PasswordChanged(val password: String): SignupIntent()
     data class ConfirmPasswordChanged(val confirmPassword: String): SignupIntent()
